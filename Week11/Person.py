b3b1d18db68ec65f9e0e0a39ec3a298a8d2a0335
Your Name <your-email@example.com>
2025-03-21 13:38:32 -0400

    WEEK 11 LAB

---
+++ Week11/Person.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, p_name, p_age, p_height):
+        print("Constructing the person object")
+        self.__name = p_name
+        self.__age = p_age
+        self.__height = p_height
+        self.public_prop = "I am a public prop"
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
+    def __del__(self):
+        print("This garbage collector will destroy everything in the end")
+
+# Instance creation and print statements moved outside the class
+person1 = Person("Rudy", 20, 176)
+print("The name of the person is: " + str(person1.name))
+person1.name = "Alfred"
+print("The name of the person is now: " + str(person1.name))
